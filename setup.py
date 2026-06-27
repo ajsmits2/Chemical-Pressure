2f5279c928693bf648c8dd4e16037e99830f857c
ajsmits2 <ajsmits2@wisc.edu>
2026-06-26 19:22:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
   author = 'Alexander Smits',                   # Type in your name
   author_email = 'ajsmits2@wisc.edu',      # Type in your E-Mail
   url = 'https://github.com/ajsmits2/Chemical-Pressure/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = https://github.com/ajsmits2/Chemical-Pressure/archive/refs/tags/Chemistry.tar.gz,    # I explain this later on
   keywords = ['Chemistry', 'Pressure', 'Chemical'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
+          'pyvista',
       ],
   classifiers=[
     'Development Status :: 5 - Production',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
