53a3d669056d3d7eeb0eca5350181dd241e8f3dd
ajsmits2 <ajsmits2@wisc.edu>
2026-06-26 20:10:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Alexander Smits',                   # Type in your name
   author_email = 'ajsmits2@wisc.edu',      # Type in your E-Mail
   url = 'https://github.com/ajsmits2/Chemical-Pressure/',   # Provide either the link to your github or to your website
-  download_url = https://github.com/ajsmits2/Chemical-Pressure/archive/refs/tags/Chemistry.tar.gz,    # I explain this later on
+  download_url = 'https://github.com/ajsmits2/Chemical-Pressure/archive/refs/tags/Chemistry.tar.gz',    # I explain this later on
   keywords = ['Chemistry', 'Pressure', 'Chemical'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
